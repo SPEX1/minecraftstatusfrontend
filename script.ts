c9a682b55fa54d986d2b479113134c2ffe4d2162
SpexFX <pimo.pk04@gmail.com>
2026-07-22 03:46:38 +0200

    - final version forgot to add a repository until end :(

---
+++ script.ts
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    getServerInformation();
+})
+const serverNameElement = document.getElementById("serverName")as HTMLElement;
+const serverAdressElement = document.getElementById("serverAdress")as HTMLElement;
+const userElement = document.getElementById("serverOnlineUsers")as HTMLElement;
+const statusIcon = document.getElementById("serverStatus")as HTMLImageElement;
+const serverIcon = document.getElementById("serverImage")as HTMLImageElement;
+const motdElement = document.getElementById("serverMotd")as HTMLElement;
+
+const serverAdressName = "Hypixel.net";
+async function getServerInformation(): Promise<void> {
+
+    try{
+        const response = await fetch(`https://api.mcsrvstat.us/3/${serverAdressName}`);
+
+        if(!response.ok){
+            throw new Error(`Error fetching server information. ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const splitAdress = serverAdressName.split(".");
+        if(!data.online){
+            serverNameElement.textContent = splitAdress[0];
+            serverAdressElement.textContent = `.` + splitAdress[1];
+            userElement.textContent = `0 / 0`;
+            statusIcon.src="./img/connectionempty.png";
+            motdElement.textContent = `Cannot reach server...`;
+        }else{
+            const playersOnline = data.players.online;
+            const playersMax = data.players.max;
+            const motd = data.motd.html.join("<br>");
+            if(data.icon){
+                serverIcon.src = data.icon;
+            }
+            serverNameElement.textContent = splitAdress[0];
+            serverAdressElement.textContent = `.` + splitAdress[1];
+            userElement.textContent = `${playersOnline} / ${playersMax}`;
+            motdElement.innerHTML = `${motd}`;
+        }
+
+
+    }
+    catch (error) {
+    }
+
+}
